de315feca2f0e72a5928f7e79685b3e90da2254b
Sergei Morozov <morozov@tut.by>
2009-12-07 16:11:11 +0200

    Дописал класс реестра и тесты к нему.

--- classes/Download/Registry.php
+++ classes/Download/Registry.php
@@ -11,13 +11,67 @@ require_once 'Download/Registry/Interface.php';
 require_once 'Download/Registry/Exception.php';
 
 /**
- *
+ * Downloaded items registry.
  */
 class Download_Registry implements Download_Registry_Interface {
 
-	protected $file;
+	/**
+	 * Line separator.
+	 */
+	const SEPARATOR = PHP_EOL;
 
+	/**
+	 * Registry file pointer.
+	 *
+	 * @var resource
+	 */
+	protected $fp;
+
+	/**
+	 * Registry data.
+	 *
+	 * @var array
+	 */
+	protected $data;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string file
+	 * @throws Download_Registry_Exception
+	 */
 	public function __construct($file) {
+
+		if (is_dir($file)) {
+			throw new Download_Registry_Exception('Directory provided');
+		}
+
+		if (!$fp = @fopen($file, 'a+')) {
+			throw new Download_Registry_Exception('Couldn\'t open registry file');
+		}
+
+		if (!@flock($fp, LOCK_EX | LOCK_NB)) {
+			throw new Download_Registry_Exception('Registry file is already in use');
+		}
+
+		$tmp = '';
+
+		while (!feof($fp)) {
+			$tmp .= fread($fp, 1024);
+		}
+
+		$this->fp = $fp;
+		$this->data = array_flip(array_filter(array_map('trim', explode(self::SEPARATOR, $tmp))));
+	}
+
+	/**
+	 * Destructor.
+	 */
+	public function __destruct() {
+		ftruncate($this->fp, 0);
+		fwrite($this->fp, implode(self::SEPARATOR, array_keys($this->data)));
+		flock($this->fp, LOCK_UN);
+		fclose($this->fp);
 	}
 
 	/**
@@ -26,7 +80,9 @@ class Download_Registry implements Download_Registry_Interface {
 	 * @param string $key
 	 * @return boolean
 	 */
-	public function isRegistered($key) {}
+	public function isRegistered($key) {
+		return isset($this->data[$key]);
+	}
 
 	/**
 	 * Registers a key.
@@ -34,5 +90,10 @@ class Download_Registry implements Download_Registry_Interface {
 	 * @param string $key
 	 * @throws Download_Registry_Exception
 	 */
-	public function register($key) {}
+	public function register($key) {
+		if (false !== strpos($key, self::SEPARATOR)) {
+			throw new Download_Registry_Exception('A key containing SEPARATOR is specified');
+		}
+		$this->data[$key] = true;
+	}
 }
--- tests/Download/RegistryTest.php
+++ tests/Download/RegistryTest.php
@@ -5,46 +5,147 @@ require_once 'PHPUnit/Framework/TestCase.php';
 require_once 'Download/Registry.php';
 
 /**
- *
+ * Download_Registry test.
  */
 class Download_RegistryTest extends PHPUnit_Framework_TestCase {
 
+	private
+		$dir = '/tmp/mfhs-test',
+		$file = null;
+
 	/**
-	 * Test construstor with non-existing file.
+	 * Sets up the fixture, for example, open a network connection.
+	 * This method is called before a test is executed.
 	 */
-	public function testConstructFileDoesntExist() {
+	protected function setUp() {
+		$this->cleanup();
+		mkdir($this->dir);
+		$this->file = $this->dir . '/' . md5(time()) . '.log';
+	}
+
+	/**
+	 * Tears down the fixture, for example, close a network connection.
+	 * This method is called after a test is executed.
+	 */
+	protected function tearDown() {
+		$this->cleanup();
+	}
+
+	/**
+	 * Cleans up test directory.
+	 */
+	protected function cleanup() {
+		if (file_exists($this->dir)) {
+			if (is_file($this->dir)) {
+				unlink($this->dir);
+			} elseif (is_dir($this->dir)) {
+				$this->rmdir($this->dir);
+			}
+		}
+	}
+
+	/**
+	 * Removes non-empty directory.
+	 */
+	protected function rmdir($path) {
+		$dir = new RecursiveDirectoryIterator($path);
+		foreach (new RecursiveIteratorIterator($dir) as $file) {
+			unlink($file);
+		}
+		foreach ($dir as $subdir) {
+			if (!@rmdir($subdir)) {
+				$this->rmdir($subdir);
+			}
+		}
+		rmdir($path);
+	}
+
+	/**
+	 * Test construstor with non-existing file and writable directory.
+	 */
+	public function testConstructNoFileDirWritable() {
+		$registry = new Download_Registry($this->file);
+		$this->assertFileExists($this->file);
+	}
+
+	/**
+	 * Test construstor with non-existing file and non-writable directory.
+	 */
+	public function testConstructNoFileDirNotWritable() {
 		$this->setExpectedException('Download_Registry_Exception');
+		$registry = new Download_Registry('/path/to/non/existing/dir');
 	}
 
 	/**
 	 * Test construstor with non-readable file.
 	 */
-	public function testConstructFileNotReadable() {
+	/*public function testConstructFileNotReadable() {
 		$this->setExpectedException('Download_Registry_Exception');
-	}
+	}*/
 
 	/**
 	 * Test construstor with non-writable file.
 	 */
-	public function testConstructFileNotWritable() {
+	/*public function testConstructFileNotWritable() {
 		$this->setExpectedException('Download_Registry_Exception');
-	}
+	}*/
 
 	/**
-	 * Test construstor with non-writable dir.
+	 * Test construstor with non-existing file and writable directory.
 	 */
-	public function testConstructDirNotWritable() {
+	public function testConstructDirectory() {
 		$this->setExpectedException('Download_Registry_Exception');
+		mkdir($this->file);
+		$registry = new Download_Registry($this->file);
 	}
 
 	/**
 	 * Test construstor with correct file.
 	 */
 	public function testConstructSuccess() {
-		try {
-			$registry = new Download_Registry(null);
-		} catch (Download_Registry_Exception $e) {
-			$this->fail($e->getMessage());
-		}
+		touch($this->file);
+		$registry = new Download_Registry($this->file);
+	}
+
+	public function testExistingKey() {
+		$registry = $this->createRegistry();
+		$this->assertTrue($registry->isRegistered('foo'));
+		$this->assertTrue($registry->isRegistered('bar'));
+	}
+
+	public function testNonExistingKey() {
+		$registry = $this->createRegistry();
+		$this->assertFalse($registry->isRegistered('baz'));
+	}
+
+	public function testRegisterValidKey() {
+		$registry = $this->createRegistry();
+		$registry->register('baz');
+		$this->assertTrue($registry->isRegistered('baz'));
+	}
+
+	public function testRegisterInvalidKey() {
+		$this->setExpectedException('Download_Registry_Exception');
+		$registry = $this->createRegistry();
+		$registry->register('baz' . PHP_EOL);
+	}
+
+	public function testPersistence() {
+		$registry1 = $this->createRegistry();
+		$registry1->register('baz');
+		unset($registry1);
+		$registry2 = new Download_Registry($this->file);
+		$this->assertTrue($registry2->isRegistered('baz'));
+	}
+
+	public function testLock() {
+		$this->setExpectedException('Download_Registry_Exception');
+		$registry1 = new Download_Registry($this->file);
+		$registry2 = new Download_Registry($this->file);
+	}
+
+	protected function createRegistry() {
+		file_put_contents($this->file, 'foo' . PHP_EOL . 'bar');
+		return new Download_Registry($this->file);
 	}
 }
