2808cfcddc92faa0d6fceaebd5dfc998a70630f5
Sergei Morozov <morozov@tut.by>
2010-01-03 23:31:19 +0200

    Вместо прописывания абсолютных путей везде, где только можно, лучше изменим рабочую директорию.

--- mfhs.php
+++ mfhs.php
@@ -1,13 +1,18 @@
 #!/usr/bin/env php
 <?php
 
-require dirname(__FILE__) . '/includes/startup.php';
+chdir(dirname(__FILE__));
+
+set_include_path(get_include_path()
+	. PATH_SEPARATOR . getenv('HOME') . '/pear/php'
+	. PATH_SEPARATOR . 'lib'
+	. PATH_SEPARATOR . 'classes');
 
 require_once 'Mfhs/Config.php';
 require_once 'Mfhs/Controller.php';
 
 try {
-	$config = new Mfhs_Config(require dirname(__FILE__) . '/config.php');
+	$config = new Mfhs_Config(require 'config.php');
 	$controller = new Mfhs_Controller($config);
 	$controller->process();
 } catch (Mfhs_Exception $e) {
