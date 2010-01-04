1b3ce9400659eaa1ca6cc1f27db70144741330cf
Sergei Morozov <morozov@tut.by>
2010-01-04 22:46:36 +0200

    Нет, смена рабочей директории не работает. Тогда путь к скачиваемому файлу должен указываться тоже относительно директории скрипта. Возвращаем всё назад.

--- mfhs.php
+++ mfhs.php
@@ -1,18 +1,18 @@
 #!/usr/bin/env php
 <?php
 
-chdir(dirname(__FILE__));
+$basedir = dirname(__FILE__);
 
 set_include_path(get_include_path()
 	. PATH_SEPARATOR . getenv('HOME') . '/pear/php'
-	. PATH_SEPARATOR . 'lib'
-	. PATH_SEPARATOR . 'classes');
+	. PATH_SEPARATOR . $basedir . '/lib'
+	. PATH_SEPARATOR . $basedir . '/classes');
 
 require_once 'Mfhs/Config.php';
 require_once 'Mfhs/Controller.php';
 
 try {
-	$config = new Mfhs_Config(require 'config.php');
+	$config = new Mfhs_Config(require $basedir . '/config.php');
 	$controller = new Mfhs_Controller($config);
 	$controller->process();
 } catch (Mfhs_Exception $e) {
